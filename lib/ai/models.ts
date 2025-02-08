f0acad8bba65ca79a61e09828dd6db292f53fafb
morethan987 <2404385626@qq.com>
2025-02-08 19:37:01 +0800

    添加chatBot模板

---
+++ lib/ai/models.ts
@@ -0,0 +1,49 @@
+import { deepseek } from '@ai-sdk/deepseek';
+import {
+  customProvider,
+  extractReasoningMiddleware,
+  wrapLanguageModel,
+} from 'ai';
+
+export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
+
+export const myProvider = customProvider({
+  languageModels: {
+    'chat-model-small': deepseek('deepseek-chat'),
+    'chat-model-large': deepseek('deepseek-chat'),
+    'chat-model-reasoning': wrapLanguageModel({
+      model: deepseek('deepseek-reasoner'),
+      middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    }),
+    'title-model': deepseek('deepseek-chat'),
+    'block-model': deepseek('deepseek-chat'),
+  },
+  // imageModels: {
+  //   'small-model': openai.image('dall-e-2'),
+  //   'large-model': openai.image('dall-e-3'),
+  // },
+});
+
+interface ChatModel {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export const chatModels: Array<ChatModel> = [
+  {
+    id: 'chat-model-small',
+    name: 'Small model',
+    description: 'Small model for fast, lightweight tasks',
+  },
+  {
+    id: 'chat-model-large',
+    name: 'Large model',
+    description: 'Large model for complex, multi-step tasks',
+  },
+  {
+    id: 'chat-model-reasoning',
+    name: 'Reasoning model',
+    description: 'Uses advanced reasoning',
+  },
+];
